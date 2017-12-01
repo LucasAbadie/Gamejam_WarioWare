0c56bd2d2047d78ab41e8ae1ba37e1640af7ae50
LucasAbadie <lucasabadie.pro@gmail.com>
2017-12-01 20:54:06 +0100

    upload Lucas Abadie 1
    + Game 08 finish
    + Add localization
    + Add parameter systems
    + Update .gitignore

--- WarioWare/Assets/Scripts/Game08Manager.cs
+++ WarioWare/Assets/Scripts/Game08Manager.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Game08Manager : MonoBehaviour {
 
+	public Text writingWord;
+	public InputField inputText;
+	public Text inputTextPlaceholder;
+
+	private string[] arrayString;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		arrayString = GameManager.GetDataParametersArray("arrayWritingWordGame08");
+
+		Debug.Log(arrayString);
+
+		writingWord.text = arrayString[Random.Range(0, 9)];
+
+		inputTextPlaceholder.text = GameManager.GetDataLanguage("inputTextPlaceholderGame08");
+
+		inputText.Select();
+		inputText.ActivateInputField();
+
 	}
+ 
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (writingWord.text == inputText.text)
+			Debug.Log("OK");
 	}
 }
